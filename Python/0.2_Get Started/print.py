93cdf22c6b580d9704ba8815be1166bbc3b36d8f
AyhamAbdalkarim <ayhamayman83@gmail.com>
2026-04-27 21:20:33 +0300

    0.4

---
+++ Python/0.2_Get Started/print.py
@@ -0,0 +1,24 @@
+print("Hello, World!")
+
+print("Hello my name is ayham")
+
+#To check the Python version of the editor, you can find it by importing the sys module:
+import sys
+
+print(sys.version)
+
+#What is the correct file extension for Python files?
+# .pp
+# .pt
+# .py
+
+#What is a correct command line syntax for checking if python is installed on your computer? (And also to check the Python version)
+# python --version
+# python ##version
+# python version
+
+#What is a correct syntax to exit the Python command line interface?
+
+# exit()
+# stop()
+# end()
---
+++ Python/0.3_Syntax/statment.py
@@ -0,0 +1,11 @@
+print("Python is fun!")
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+#Invalid Syntax
+print("Python is fun!") print("Really!")
